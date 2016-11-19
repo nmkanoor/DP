dc1551d6e244ff479f9c99641cf6e8559370bd3e
Nikhil Kanoor <nk352@afsconnect2.njit.edu>
2016-11-19 14:20:25 -0500

    abstract.php

---
+++ abstract.php
@@ -0,0 +1,30 @@
+<?php
+abstract class Book {
+	protected $title;
+	protected $content;
+	
+	function setTitle( $str )
+	{
+		$this -> title = $str;
+	}
+	function setContent( $str )
+	{
+		$this -> content = $str;
+	}
+}
+class Paperback extends Book {
+	function printBook()
+	{
+		var_dump("The book '{$this -> title}' was printed.");
+	}
+}
+class eBook extends Book {
+	function generatePDF()
+	{
+		var_dump("A PDF was generated for this eBook '{$this -> title}'");
+	}
+}
+$paperback = new Paperback;
+$paperback -> setTitle("The Greatest Paperback Ever");
+$paperback -> printBook();
+
